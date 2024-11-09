c73b180895962f21864fc4f5648b8b452e690572
rezvanova <an_rezvanova@mail.ru>
2024-11-09 14:59:23 +0300

    add

--- utils/search_notebooks.py
+++ utils/search_notebooks.py
@@ -3,24 +3,26 @@ from whoosh import index
 from whoosh.qparser import QueryParser
 
 class IndexSearcher:
-    def __init__(self, index_name):
+    def __init__(self, index_path):
         """
-        Инициализирует поисковый объект, открывая индекс.
-        
+        Инициализирует поисковый объект, открывая индекс по указанному пути.
+
         Параметры:
-        - index_name: Название папки с индексом.
+        - index_path: Полный путь к папке с индексом.
         """
-        idx_path = os.path.join("indexes", index_name)
-        self.idx = index.open_dir(idx_path)  # Открываем указанный индекс
+        if not os.path.exists(index_path):
+            raise ValueError(f"Указанный путь {index_path} не существует.")
+        
+        self.idx = index.open_dir(index_path)  # Открываем указанный индекс
         self.searcher = self.idx.searcher()   # Создаем поисковик, который будет открыт на протяжении сеанса
 
     def search(self, search_word):
         """
         Выполняет поиск по индексу и возвращает исходные пути к файлам с совпадением.
-        
+
         Параметры:
         - search_word: Слово или фраза для поиска.
-        
+
         Возвращает:
         - Список исходных путей к файлам, где найдено совпадение.
         """
@@ -34,15 +36,3 @@ class IndexSearcher:
     def close(self):
         """Закрывает поисковик."""
         self.searcher.close()
-
-# Пример использования
-
-# Создаем поисковый объект (открывает индекс)
-index_searcher = IndexSearcher("phds_fall_2023")  # Замените на нужное имя индекса
-
-# Выполняем несколько запросов без закрытия поисковика
-print(index_searcher.search("ваше_слово_для_поиска"))
-print(index_searcher.search("другое_слово_для_поиска"))
-
-# Закрываем поисковик, когда больше не нужен
-index_searcher.close()
